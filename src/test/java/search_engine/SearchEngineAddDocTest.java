506506f7ccdf4305e20391295578c7915fe1fbdb
alisad <asgh1381@gmail.com>
2024-07-06 15:47:22 +0330

    refactor addDocument test

--- src/test/java/search_engine/SearchEngineAddDocTest.java
+++ src/test/java/search_engine/SearchEngineAddDocTest.java
@@ -1,24 +1,48 @@
 package search_engine;
 
 import org.junit.jupiter.api.Test;
+import search_engine.filters.NumberFilter;
+import search_engine.filters.PunctuationFilter;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.*;
 
 public class SearchEngineAddDocTest {
 
-    SearchEngine underTest;
-
-
-
-
-
-
-
-
     @Test
-    public void addData_givenNullInput_notThrowException() {
-        assertDoesNotThrow(() -> underTest.addDocument(null),
+    public void addDocument_givenNullInput_notThrowException() {
+        //given
+        SearchEngine searchEngine = SearchEngine.builder()
+                .build();
+
+        //then
+        assertDoesNotThrow(() -> searchEngine.addDocument(null),
                 "addData should not throw NullPointerException");
     }
 
+    @Test
+    public void addDocument_givenMockFilters_invokeFilters() {
+        //given
+        NumberFilter mockNumberFilter = mock(NumberFilter.class);
+        PunctuationFilter mockPunctuationFilter = mock(PunctuationFilter.class);
+        when(mockNumberFilter.filter(anyString())).thenReturn("");
+        when(mockPunctuationFilter.filter(anyString())).thenReturn("");
+
+        //when
+        SearchEngine searchEngine = SearchEngine.builder()
+                .filters(List.of(mockNumberFilter, mockPunctuationFilter))
+                .build();
+
+        Document document = Document.builder()
+                .id("1")
+                .content("")
+                .build();
+        searchEngine.addDocument(document);
+
+        //then
+        verify(mockNumberFilter).filter(anyString());
+        verify(mockPunctuationFilter).filter(anyString());
+    }
 }
