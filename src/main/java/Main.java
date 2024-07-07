fccc333c831fde176a7629fb4e7040e4f415c1a3
alisad <asgh1381@gmail.com>
2024-07-07 11:32:28 +0330

    add LowerCaseFilter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import search_engine.Document;
 import search_engine.SearchEngine;
+import search_engine.filters.LowerCaseFilter;
 import search_engine.query_decoder.CommonQueryDecoder;
 import search_engine.tokenizers.SpaceTokenizer;
 
@@ -7,14 +8,15 @@ import java.io.File;
 import java.util.*;
 
 public class Main {
-    private final static String DIR_PATH = "src/main/resources/docs";
+    private final static String DOCUMENTS_DIRECTORY = "src/main/resources/docs";
 
     public static void main(String[] args) {
-        List<File> files = FileUtilities.getFilesByDirPath(DIR_PATH);
+        List<File> files = FileUtilities.getFilesByDirPath(DOCUMENTS_DIRECTORY);
         List<Document> documents = getDocuments(files);
 
 
         SearchEngine searchEngine = SearchEngine.builder()
+                .filters(List.of(new LowerCaseFilter()))
                 .tokenizer(new SpaceTokenizer())
                 .queryDecoder(new CommonQueryDecoder())
                 .build();
@@ -33,18 +35,13 @@ public class Main {
         List<Document> documents = new ArrayList<>();
         for (File file : files) {
             String content = FileUtilities.readFileContent(file);
-            String normalizedContent = normalizeStr(content);
             Document document = Document.builder()
                     .id(file.getName())
-                    .content(normalizedContent)
+                    .content(content)
                     .build();
             documents.add(document);
         }
         return documents;
-    }
-
-    private static String normalizeStr(String content) {
-        return content.toLowerCase().replaceAll("\n", " ");
     }
 
     private static void printSet(Set<String> set) {
