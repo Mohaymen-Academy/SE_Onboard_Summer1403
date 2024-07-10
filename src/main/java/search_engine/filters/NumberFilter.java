ce5dbb725558c70370aa5d98a5c4e785925968c4
omid <omidheydari2004@gmail.com>
2024-07-10 11:46:50 +0330

    0-9 -> \\d

--- src/main/java/search_engine/filters/NumberFilter.java
+++ src/main/java/search_engine/filters/NumberFilter.java
@@ -3,6 +3,6 @@ package search_engine.filters;
 public class NumberFilter implements Filter {
     @Override
     public String filter(String str) {
-        return str.replaceAll("[+|-]?[0-9]+", "");
+        return str.replaceAll("[+|-]?\\d+", "");
     }
 }
